6abbeda9bc1c09de05f37aaca7fbdfc21fb786ef
AndreaPotenza <andreapoten3@gmail.com>
2024-11-15 16:15:14 +0100

    funzione nomi

---
+++ names.js
@@ -0,0 +1,5 @@
+function peopleNames(firstName, lastName) {
+    return {firstName, lastName}
+}
+
+module.exports = peopleNames
---
+++ people.js
@@ -0,0 +1,4 @@
+const peopleNames = require("./names.js")
+
+const name = peopleNames('andrea', 'potenza')
+console.log(name)
