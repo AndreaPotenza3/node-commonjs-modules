5b088ab064221e1e1eedad79197c43393cf9a479
AndreaPotenza <andreapoten3@gmail.com>
2024-11-15 16:27:29 +0100

    hobbies

---
+++ hobbies.js
@@ -0,0 +1,6 @@
+function peopleHobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    // return {[{hobbyOne},{hobbyTwo},{hobbyThree}]}
+    return {hobbies:[hobbyOne, hobbyTwo, hobbyThree]}
+}
+
+module.exports = peopleHobbies
--- people.js
+++ people.js
@@ -1,4 +1,8 @@
 const peopleNames = require("./names.js")
+const peopleHobbies = require("./hobbies.js")
 
 const name = peopleNames('andrea', 'potenza')
 console.log(name)
+
+const hobby = peopleHobbies('collezionare francobolli','equtazione','falegnameria')
+console.log(hobby)
